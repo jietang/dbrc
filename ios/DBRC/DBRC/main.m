e8ce64a6b9491f0d0e9a424567dcc9dd73356a85
Robert Luedeman <robert.luedeman@gmail.com>
2013-01-06 21:51:51 -0500

    xcode scaffold

---
+++ ios/DBRC/DBRC/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DBRC
+//
+//  Created by luedeman on 1/6/13.
+//  Copyright (c) 2013 Dropbox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DBAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DBAppDelegate class]));
+    }
+}
