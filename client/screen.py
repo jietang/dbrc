72c78e03c2a385e8d30db2cd826d7fda4f753644
Robert Luedeman <robert.luedeman@gmail.com>
2013-01-08 16:00:21 -0800

    updated screen.py to use the new api methods

--- client/screen.py
+++ client/screen.py
@@ -1,23 +1,25 @@
 from api_util import request
 import webbrowser
 import time
+import requests
 
 ID=1
 
-screen_id = request('register', ID)['screen_id']
+screen_id = requests.post('http://127.0.0.1:5000/screens/').json()['screen_id']
 print "current screen id: ", screen_id
 
 while True:
     try:
-        resp = request('subscribe', screen_id)
+        print 'subscribing as screen %s...' % (screen_id,)
+        resp = requests.get('http://127.0.0.1:5000/screens/%s' % (screen_id,))
     except Exception:
         time.sleep(1)
         continue
 
-    if resp['result'] == 'resubscribe':
+    if resp.json()['result'] == 'resubscribe':
         print "resubscribing"
         continue
-    elif resp['result'] == 'ok':
-        print "Launching: ", resp['data']
-        webbrowser.open(resp['data'])
+    elif resp.json()['result'] == 'ok':
+        print "Launching: ", resp.json()['data']
+        webbrowser.open(resp.json()['data'])
         continue
