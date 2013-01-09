681815e06d3acb389accb808c07777fafc8f142a
Robert Luedeman <robert.luedeman@gmail.com>
2013-01-08 17:21:36 -0800

    update remote script to work with new api

--- client/remote.py
+++ client/remote.py
@@ -1,13 +1,22 @@
 import sys
 import time
+import json
 
-from api_util import request
+import requests
 
 url = 'http://www.xkcd.com'
 if len(sys.argv) > 2:
     url = sys.argv[2]
 
-broadcast_id = request('create_broadcast')['broadcast_id']
-request('add_to_broadcast', broadcast_id, int(sys.argv[1]))
+screen_id = int(sys.argv[1])
+broadcast_id = requests.post(url='http://127.0.0.1:5000/broadcasts/').json()['broadcast_id']
+print 'have broadcast with id', broadcast_id
+r = requests.post('http://127.0.0.1:5000/broadcasts/%s/screens/' % broadcast_id, data={'screen_id': screen_id})
+if r.status_code != 200:
+    print 'failed to add %s to broadcast %s' % (screen_id, broadcast_id)
+    exit(1)
+
+print 'added screen %s to broadcast %s' % (screen_id, broadcast_id)
 time.sleep(1.0)
-request('push', broadcast_id, url)
+r = requests.post('http://127.0.0.1:5000/broadcasts/%s' % broadcast_id, data=json.dumps({'url': url}), headers={'content-type': 'application/json'})
+print 'pushed %s to broadcast %s' % (url, broadcast_id)
