e4ff02f67739573b17df9df9722aa84bba150b0a
Jie Tang <jie@mbpr411-10.corp.dropbox.com>
2013-01-08 17:31:07 -0800

    minor fixes to screen.py

--- client/api_util.py
+++ client/api_util.py
@@ -1,7 +1,9 @@
 import requests
+import os
 
-HOST='127.0.0.1'
-PORT=5000
+is_prod = os.getenv('IS_PROD', False)
+HOST = 'ec2-54-235-229-59.compute-1.amazonaws.com' if is_prod else '127.0.0.1'
+PORT = 5000 if is_prod else 5000
 
 def request(rpc_name, *args):
     rpc_target = 'http://%s:%d/%s' % (HOST, PORT, rpc_name)
