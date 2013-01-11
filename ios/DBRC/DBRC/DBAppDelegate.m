68453c3b888f09bd52651fc0c67f1ec7f51f5a38
Jie Tang <jie@mbpr411-10.corp.dropbox.com>
2013-01-11 03:33:17 -0800

    image browser

--- ios/DBRC/DBRC/DBAppDelegate.m
+++ ios/DBRC/DBRC/DBAppDelegate.m
@@ -9,6 +9,7 @@
 #import "DBAppDelegate.h"
 #import "DBPhotosViewController.h"
 #import "DBPairingViewController.h"
+#import "GalleryDelegate.h"
 
 @implementation DBAppDelegate
 
@@ -21,10 +22,16 @@
     
     // Basic Search Controller is on bottom of the stack. Slide the Pairing
     // controler on top.
-    UIViewController *rootViewController = [[DBPhotosViewController alloc] init];
-    UINavigationController *navController = [[UINavigationController alloc] initWithRootViewController:rootViewController];
+    //UIViewController *rootViewController = [[DBPhotosViewController alloc] init];
+    //UINavigationController *navController = [[UINavigationController alloc] initWithRootViewController:rootViewController];
     
-    [self.window setRootViewController:navController];
+    //[self.window setRootViewController:navController];
+    
+    // new hotness
+    self.photoSource = [[GalleryDelegate alloc] init];
+    FGalleryViewController *gallery = [[FGalleryViewController alloc] initWithPhotoSource:self.photoSource];
+    UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:gallery];
+    [self.window setRootViewController:nav];
     
     return YES;
 }
